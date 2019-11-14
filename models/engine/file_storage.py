7096ce3eb2f10bec948428fc013b72e9efbb1884
Caroll1889 <diahancaroll@hotmail.com>
2019-11-14 04:05:23 +0000

    fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,24 +29,24 @@ class FileStorage():
     def new(self, obj):
         """sets in __objects the obj with key"""
         key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serialize __objects to de JSON file """
         j_objects = {}
-        for key in self.__objects:
-            j_objects[key] = self.__objects[key].to_dict()
+        for key in FileStorage.__objects:
+            j_objects[key] = FileStorage.__objects[key].to_dict()
         with open(self.__file_path, "w") as file:
             json.dump(j_objects, file)
 
     def reload(self):
         """deserialize the JSON file to __objects """
         try:
-            with open(self.__file_path) as file:
+            with open(FileStorage.__file_path) as file:
                 reloaded = json.loads(file.read())
                 for key, value in reloaded.items():
                     class_name = key.split('.')[0]
                     obj = models.classes[class_name](**value)
-                    self.__objects[key] = obj
+                    FileStorage.__objects[key] = obj
         except:
             pass
